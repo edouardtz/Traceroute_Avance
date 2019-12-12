96f464737557b71508d2855d41c5466cf5bd300a
root <root@244-0>
2019-12-12 18:02:46 +0100

    Fin de seance 12.12.2019 - TOUT OK - trvail sur alignement résultat de la console en cours

--- traceroute-2.sh
+++ traceroute-2.sh
@@ -11,40 +11,45 @@
 > traceroute.txt                                    # Nettoyage du fichier contenant le résultat du script
 > traceroute.dot                                    # Nettoyage du fichier dot contenant le graphe
 echo "digraph traceroute { " > traceroute.txt       # Début du fichier dot
-echo ""                                             # Saut de ligne
 
 #-----------------------------------Variables----------------------------------#
                                               
 ttl=1                                               # Variable du TTL pour commencer au premier HOP 
 arg="0"                                             # Position dans le tableau des options arguments
-#web="0"                                             # Position dans le tableau des adresses web
+#web="0"                                            # Position dans le tableau des adresses web
 boucle='0'                                          # Boucle infinie dans le "while" principal
 anti_doublon=""                                     # Correcteur de doublon           
 options=("" "-I" "-T"  "-p 53 -U" "-p 67 -U" "-p 25 -T" "-p 80 -T" "-p 443 -T" "-p 21 -T" "-p 22 -T" "-p 53 -T" "end") # Liste des options à tester en argument de la commande traceroute     
 sites=(
-    "iutp.univ-poitiers.fr"
-    "rt.unice.fr"
-    "cfasup-fc.com"
-    "www.chine-nouvelle.com"
-    "www.mines-ales.fr")
+ #   "perdu.com"
+ #   "rt.unice.fr"
+ #   "cfasup-fc.com"
+ #   "chine-nouvelle.com"
+ #   "mines-ales.fr"
+    "iut.fr"
+    "onisep.fr"
+    "global.bfsu.edu.cn")                           # Liste des adresses web a consulter
 
 #----------------------------------TRACEROUTE----------------------------------#
 for siteweb in "${sites[@]}" ; do
-    hops=$(traceroute $siteweb -w 1 | wc -l)       # Nombre de lignes du traceroute        
-    echo "SITE WEB EN COURS :" $siteweb
+    ipaddr=$(nslookup $siteweb | grep Address | tail -n 1 | cut -d " " -f 2)
+    #masque=$(ipcalc $ipaddr | grep Netmask | awk '{print($4)}'  )              #Extraction du masque de sous-réseau  
+    hops=$(traceroute $siteweb -w 1 | wc -l)                                    # Nombre de lignes du traceroute   
+    #AS=$(traceroute -n -f $ttl -m $ttl -w 1 -q 1 $siteweb -A | tail -n 1 | awk '{print($3)}')
+    echo "AS : $AS"       
+    echo -e "\e[96m Cible :\e[0m" $siteweb "($ipaddr)" | column -t
     while [ "$ttl" != "$hops" ] ; do                                            # Boucle Principale / s'arrête quand le nombre de hops calculé auparavant est atteint                                              
         while [ "$boucle" == '0' ]  ; do                                        # Boucle infinie tant que les conditions futures ne "breakent" pas
-            resultat=$(traceroute -n -f $ttl -m $ttl -w 2 -q 1 $siteweb ${options[arg]} -A | tail -n 1 | awk '{print($2,$3)}')         #Résultat de la commande traceroute stockée, en fonction des options utilisées          
-            if [ "$resultat" != "* " ] ; then                                  # Condition si on trouve un résultat non nul
+            resultat=$(traceroute -n -f $ttl -m $ttl -w 1 -q 1 $siteweb ${options[arg]} -A | tail -n 1 | awk '{print($2,$3)}')         #Résultat de la commande traceroute stockée, en fonction des options utilisées          
+            if [ "$resultat" != "* " ] ; then                                   # Condition si on trouve un résultat non nul
                 if [ "$anti_doublon" == "$resultat" ] ; then                    # Condition si c'est un doublon
                     break   
                 else
                     if [ "$ttl" != "1" ] ; then                                 # Condition si c'est la première ligne
                         echo " -> " >> traceroute.txt
                     fi
-                    
-                    echo " \" $ttl $resultat \" " >> traceroute.txt             # Afficher le résultat (si le résultat n'est pas nul et que ce n'est pas un doublon)
-                    echo -e "\e[32m $ttl \e[0m" "$resultat"
+                    echo " \" $ttl $resultat\" " >> traceroute.txt             # Stocker le résultat (si le résultat n'est pas nul et que ce n'est pas un doublon)
+                    echo -e "\e[92m $ttl:\e[0m$resultat" | column -s : -t               # Afficher le résultat
                     ((arg=arg+1))                                               # Incrémentation de la position dans le tableau des options
                     break
                 fi
@@ -52,21 +57,22 @@ for siteweb in "${sites[@]}" ; do
                 #echo "$ttl" "$resultat" "${options[arg]}"
                 ((arg=arg+1))                                                   # Changement d'option traceroute  
                 if [ "${options[arg]}" == "end" ] ; then                        # Si on arrive à la fin du tableau des options
-                    echo "->" >> traceroute.txt
-                    echo "\" $ttl 'Not found' \"" >> traceroute.txt
-                    echo -e "\e[31m $ttl \e[0m" "¯\_(ツ)_/¯"
+                    echo "->" >> traceroute.txt                                 # Ecrire la flèche
+                    echo "\" $ttl Not found \"" >> traceroute.txt               # Ecrire le résultat dans le fichier
+                    echo -e "\e[91m $ttl \e[0m" "¯\_(ツ)_/¯" | column -t                # Afficher l'erreur
                     break
                 fi
             fi
         done
         ((ttl=ttl+1))                                                           # Augmenter le TTL pour passer au HOP suivant
-        arg="0"                                                                  # Réinitialiser la position dans le tableau
+        arg="0"                                                                 # Réinitialiser la position dans le tableau
         anti_doublon="$resultat"                                                # Stockage du résultat pour comparer avec le résultat futur      
     done
-    echo "->" "$siteweb" >> traceroute.txt
+    #echo "->" \"$siteweb\" >> traceroute.txt                                    # Afficher l'adresse cible à la fin du graphe
     ttl="1"
-done          
-#----------------------------------Mise en forme graphe----------------------------------#
+done
+
+##----------------------------------Mise en forme graphe----------------------------------#
 
 echo ""                                                 # Saut de ligne
 echo ";" >> traceroute.txt                              # Fin du schéma dot
