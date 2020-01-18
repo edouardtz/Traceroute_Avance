7e79f18ba42716a221075387b9420e26c63fa371
edouard-tz <edouardtouzan@gmail.com>
2020-01-18 08:32:39 -0500

    Pret à rendre

---
+++ essais/TCRT.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# - - - - - - - - - - VARIABLES - - - - - - - - - - #
+
+siteweb="$1"                                        
+ttl=1                                               
+arg="0"                                             
+boucle='0'                                          
+ip_precedente=""                                     
+options=("" "-I" "-T"  "-p 53 -U" "-p 67 -U" "-p 25 -T" "-p 80 -T" "-p 443 -T" "-p 21 -T" "-p 22 -T" "-p 53 -T" "end") 
+ip_finale=$(nslookup $siteweb| grep Address | tail -n 2 | awk '{print($2)}' | grep -v "#" | grep -v ":")
+hops=$(traceroute $siteweb -w 1 | wc -l)            
+
+# - - - - - - - - - - SCRIPT - - - - - - - - - - #
+
+                                   
+echo ""
+echo -e "\e[1;96m Cible :\e[0m" $siteweb "($ip_finale)" "| N°$(($3+1))/$4"
+while [ "$ip_actuelle" != "$ip_finale" ] ; do                                            
+    while [ "$boucle" == '0' ]  ; do                                        
+        resultat="$(traceroute -n -f $ttl -m $ttl -w 1 -q 1 $siteweb ${options[arg]} -A | tail -n 1 | awk '{print($2,$3)}')"         
+        ip_actuelle="$(traceroute -n -f $ttl -m $ttl -w 1 -q 1 $siteweb ${options[arg]} | tail -n 1 | awk '{print($2)}')"
+        echo "ip act = $ip_actuelle"
+        if [ "$resultat" != "* " ] ; then                                   
+            if [ "$ip_precedente" == "$ip_actuelle" ] ; then
+                echo "doublon mdr"
+                break
+            else
+                echo "pas doublon"
+                if [ "$ttl" != "1" ] ; then                                
+                    echo "-> " >> $siteweb.txt
+                fi  
+                echo "\"$ttl $resultat\" " >> $siteweb.txt                 
+                echo -e "\e[92m $ttl    \e[0m$resultat      "
+                break
+            fi
+        else    
+            if [ "$ttl" != "1" ] ; then                                
+                echo "-> " >> $siteweb.txt
+            fi        
+            if [ "$anti_doublon" == "$resultat" ] ; then                    
+                echo -e "\e[92m $ttl    \e[0m$resultat      DOUBLON"
+                echo " \"$ttl DOUBLON $resultat\" " >> $siteweb.txt  
+                break
+            else
+                echo "\"$ttl  $resultat\" " >> $siteweb.txt                 
+                echo -e "\e[92m $ttl   \e[0m$resultat      "               
+                ((arg=arg+1))                                               
+                break
+            fi                                                                       
+            ((arg=arg+1))                                                   
+            if [ "${options[arg]}" == "end" ] ; then                                                         
+                echo " -> \"$ttl Not found ($siteweb)\"" >> $siteweb.txt        
+                echo -e "\e[91m $ttl \e[0m" "   ¯\_(ツ)_/¯"                
+                break
+            fi
+        fi
+    done
+    ((ttl=ttl+1))                                                          
+    arg="0"                                                                 
+    ip_precedente="$ip_actuelle"
+    anti_doublon="$resultat"                                                
+done
+
+# - - - - - - - - - - CALCUL TEMPS - - - - - - - - - - #
+
+temps_ecoule=$SECONDS
+if [ $temps_ecoule -lt 60 ] ; then                                                                     
+    echo -e "\e[93m         » Cible atteinte en $SECONDS secondes.\e[0m"                                
+else
+    echo -e "\e[93m         » Cible atteinte en $((($SECONDS/60)%60)) minute(s), $(($SECONDS%60)) secondes.\e[0m"
+fi
+
+# - - - - - - - - - - ACHEVEMENT - - - - - - - - - - #
+
+echo "-> \"$siteweb\" [shape=box,color=$2];" >> $siteweb.txt                
+tr -d '\n' < $siteweb.txt > $siteweb.rte                                    
